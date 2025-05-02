e27fb4507974087f7d15a5026ede84d49ceb8255
tusharrajora511 <tusharrajora72@gmail.com>
2025-05-02 15:44:32 +0530

    MiddleWare

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+Route::get("/login", function(){
+    return view('login');
+});
 
-Route::view("/","login");
+Route::post("/login", [UserController::class,'login']);
+Route::get("/product", [ProductController::class,'index']);
