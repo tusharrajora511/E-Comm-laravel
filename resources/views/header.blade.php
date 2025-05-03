61fa032bdee7f4e55610715df648aa58b3318f11
tusharrajora511 <tusharrajora72@gmail.com>
2025-05-03 12:50:17 +0530

    login and logout

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,10 @@
 <?php
 use App\Http\Controllers\ProductController;
-
+$total =0;
+if(Session::has('user')){
+  
 $total =ProductController::cartItem();
+}
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
@@ -30,7 +33,22 @@ $total =ProductController::cartItem();
           <li class="nav-item">
             <a class="nav-link" href="#">Cart({{$total}})</a>
           </li>
+          @if(Session::has('user'))
+          <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+{{Session::get('user')['name']}}  </a>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="/logout">Logout</a></li>
+    <li><a class="dropdown-item" href="#">Page 1-2</a></li>
+    <li><a class="dropdown-item" href="#">Page 1-3</a></li>
+  </ul>
+</li>
+@else
+<li><a class="nav-link" href="/login">Login</a></li>
+@endif
+
         </ul>
+        
       </div>
     </div>
   </nav>
